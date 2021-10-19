ee0386b3b0f0a06eeaaabc71e9b0d1b8a88c8657
Matthew J Martin <19975+matmar10@users.noreply.github.com>
2021-10-18 21:53:51 -0400

    fix: inconsistent github token param

--- dist/index.js
+++ dist/index.js
@@ -1531,7 +1531,7 @@ const validEvent = ['pull_request'];
 
 async function run() {
   try {
-    const token = core.getInput('github-token', { required: true });
+    const token = core.getInput('token', { required: true });
 
     const octokit = new github.getOctokit(token);
 
@@ -1599,72 +1599,6 @@ async function run() {
 }
 
 run();
-
-//
-//
-// // config.rules['references-empty'] = [2, 'never'];
-//
-// const validEvent = ['pull_request'];
-//
-// async function main() {
-//   try {
-//
-//     const {
-//       eventName,
-//       payload: {
-//         repository: repo,
-//         pull_request: pr
-//       }
-//     } = github.context;
-//
-//     const token = core.getInput('github-token', {required: true});
-//     const debug = core.getInput('debug');
-//     const userAgent = core.getInput('user-agent');
-//     const previews = core.getInput('previews');
-//
-//     const opts = {};
-//     if (debug === 'true') {
-//       opts.log = console;
-//     }
-//     if (userAgent != null) {
-//       opts.userAgent = userAgent;
-//     }
-//     if (previews != null) {
-//       opts.previews = previews.split(',');
-//     }
-//
-//     if (validEvent.indexOf(eventName) < 0) {
-//       core.error(`Invalid event: ${eventName}`);
-//       return;
-//     }
-//
-//     const github = getOctokit(token, opts);
-//
-//     const commits = await github.rest.pulls.listCommits({
-//       owner: repo.owner.login,
-//       repo: repo.name,
-//       pull_number: pr.number,
-//     })
-//
-//     console.log(commits);
-//
-//     core.setOutput('commits', JSON.stringify(commits.data))
-//
-//
-//     // lint(`fix: bad bug
-//     //
-//     // fixes #243
-//     // `, config.rules)
-//     //   .then((report) =>
-//     //     console.log(report)
-//     //   );
-//
-//   } catch (error) {
-//     core.setFailed(error.message)
-//   }
-// }
-//
-// main()
 
 
 /***/ }),
