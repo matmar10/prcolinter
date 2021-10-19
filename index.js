60bc88cfdc19d9dfb3f553a339e6e9bdb29caa6b
Matthew J Martin <19975+matmar10@users.noreply.github.com>
2021-10-19 13:11:10 -0400

    feat: comment on pull request with lint report
    Fixes #14

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
 const lintLib = require('@commitlint/lint');
 const defaultConfig = require('@commitlint/config-conventional');
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 const path = require('path');
+const moment = require('moment');
 
 const lint = lintLib.default;
 const defaultConfigRules = defaultConfig.rules;
@@ -15,6 +18,7 @@ const validEvent = ['pull_request'];
 async function run() {
   try {
     const token = core.getInput('token', { required: true });
+    const createComment = core.getBooleanInput('comment');
 
     // load rules from file, if there
     const configPath = core.getInput('config_path', { required: true });
@@ -41,6 +45,12 @@ async function run() {
         repository: repo,
         pull_request: pr,
       },
+      issue: {
+        number: num,
+      },
+      repo: {
+        owner,
+      },
     } = github.context;
 
     if (validEvent.indexOf(eventName) < 0) {
@@ -57,28 +67,127 @@ async function run() {
     const reports = await Promise.all(commits.data.map(commit => lint(commit.commit.message, ruleSet)));
     let countErrors = 0;
     let countWarnings = 0;
+    const authors = [];
+
+    const commitReports = [];
     reports.forEach((report, i) => {
       const meta = commits.data[i];
       const { sha, commit } = meta;
-      core.startGroup(`Commit "${commit.message}" ${sha.substring(0, 7)} (${commit.author.name} <${commit.author.email}> on ${commit.author.date})`);
+      if (!authors.includes(commit.author.name)) {
+        authors.push(commit.author.name);
+      }
+      const shaShort = sha.substring(0, 7);
+      const relativeTime = moment(commit.author.date).fromNow();
+
+      core.startGroup(`Commit "${commit.message}" ${shaShort} (${commit.author.name} <${commit.author.email}> on ${commit.author.date})`);
+
+      const headerIcon = report.valid ? '✅' :
+        report.errors.length ? '❌' : '⚠️';
+      let commitReportText = `
+### ${headerIcon} [Commit ${shaShort}](https://github.com/${owner}/${repo.name}/commit/${sha})
+
+By **[${commit.author.name} (${meta.committer.login})](https://github.com/${meta.committer.login})** _${relativeTime}_
+
+\`\`\`
+${commit.message}
+\`\`\`
+`;
+
       if (!report.valid) {
+        let errorReportText = '';
         report.errors.forEach((err) => {
+          const ruleDef = ruleSet[err.name];
           core.error(`Rule '${err.name}': ${err.message} ("${commit.message}")`);
           countErrors++;
+          errorReportText += `
+❌ **ERROR**: ${err.message}
+> ["${err.name}"](https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md#${err.name}): ${JSON.stringify(ruleDef)}
+`;
         });
+
+        let warningReportText = '';
         report.warnings.forEach((err) => {
+          const ruleDef = ruleSet[err.name];
           core.warning(`Rule '${err.name}': ${err.message} ("${commit.message}")`);
           countWarnings++;
+          warningReportText += `
+⚠️ **Warning**: ${err.message}
+> ["${err.name}"](https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md#${err.name}): ${JSON.stringify(ruleDef)}
+`;
         });
+        commitReportText += `
+${errorReportText}
+${warningReportText}
+`;
       }
+      commitReports.push(commitReportText);
       core.endGroup();
     });
 
     if (countErrors) {
       core.setFailed(`Action failed with ${countErrors} errors (and ${countWarnings} warnings)`);
+      const finalReport = `
+# 🚨🚔 Unconventional Commit 👮‍♀️🙅‍♂️
+
+🤖 Beep boop! Looks like one or more of your commit messages wasn't quite right.
+
+## Commit Message Lint Report
+
+- ✏️  **${commits.data.length} commit(s)**
+- 👤  **${authors.length} author(s)**
+- ❌  **${countErrors} lint error(s)**
+- ⚠️  **${countWarnings} lint warning(s)**
+
+${commitReports.join('\n')}
+
+## Tips
+
+Be sure to follow the [Conventional Commit](https://www.conventionalcommits.org/en/v1.0.0/) guideline when authoring your commits.
+
+If your most recent commit is to blame, you can edit a single commit with:
+
+\`\`\`
+git commit --amend
+\`\`\`
+
+To edit & merge multiple commits, you can rebase with \`git rebase -i master\` (be sure your master is up to date).
+
+      `;
+
+      if (createComment) {
+        if (countErrors || countWarnings) {
+          await octokit.rest.issues.createComment({
+            owner,
+            repo: repo.name,
+            issue_number: num,
+            body: finalReport,
+          });
+        } else {
+          await octokit.rest.issues.createComment({
+            owner,
+            repo: repo.name,
+            issue_number: num,
+            body: `
+# ✅🙏🏻 Conventional Commit 🥳 🎉
+
+🤖 Beep boop! Congrats, it like all your commit messages conform to the [Conventional Commit](https://www.conventionalcommits.org/en/v1.0.0/) spec! 👏👏👏
+
+Your PR can be closed. Coffee is for closers, so here's a coffee for you: ☕️
+
+## Commit Message Lint Report
+
+- ✏️  **${commits.data.length} commit(s)**
+- 👤  **${authors.length} author(s)**
+- ❌  **${countErrors} lint error(s)**
+- ⚠️  **${countWarnings} lint warning(s)**
+`,
+          });
+        }
+      }
     }
-  } catch (error) {
-    core.setFailed(error.message);
+  } catch (err) {
+    console.error(err);
+    core.setFailed(err.message);
   }
 }
 
