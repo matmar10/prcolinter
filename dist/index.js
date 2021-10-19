4880aa25ae120dc87fe47e8ca8681ba7e077fbbf
Matthew J Martin <19975+matmar10@users.noreply.github.com>
2021-10-18 23:01:23 -0400

    feat: specify rules in action

--- dist/index.js
+++ dist/index.js
@@ -1532,6 +1532,8 @@ const validEvent = ['pull_request'];
 async function run() {
   try {
     const token = core.getInput('token', { required: true });
+    const rulesRaw = core.getInput('rules');
+    const rules = rulesRaw ? JSON.parse(rulesRaw) : {};
 
     const octokit = new github.getOctokit(token);
 
@@ -1543,33 +1545,30 @@ async function run() {
       }
     } = github.context;
 
-    console.log({
-      eventName,
-      repo,
-      pr
-    });
-
     if (validEvent.indexOf(eventName) < 0) {
       core.error(`Invalid event: ${eventName}`);
       return;
     }
+
+    const ruleSet = {
+      ...config.rules,
+      ...rules
+    };
 
     const commits = await octokit.rest.pulls.listCommits({
       owner: repo.owner.login,
       repo: repo.name,
       pull_number: pr.number,
     });
-    const reports = await Promise.all(commits.data.map(commit => lint(commit.commit.message, config.rules)));
-    const output = [];
+    const reports = await Promise.all(commits.data.map(commit => lint(commit.commit.message, ruleSet)));
+
     let countErrors = 0;
     let countWarnings = 0;
     reports.forEach((report, i) => {
       const meta = commits.data[i];
       const { sha, commit } = meta;
       core.startGroup(`Commit "${commit.message}" ${sha.substring(0, 7)} (${commit.author.name} <${commit.author.email}> on ${commit.author.date})`);
-      if (report.valid) {
-        core.notice('OK');
-      } else {
+      if (!report.valid) {
         report.errors.forEach(err => {
           core.error(`Rule '${err.name}': ${err.message} ("${commit.message}")`);
           countErrors++;
@@ -1579,15 +1578,8 @@ async function run() {
           countWarnings++;
         });
       }
-      output.push({
-        sha,
-        commit,
-        report
-      })
       core.endGroup();
     });
-
-    // core.setOutput('report', output);
 
     if (countErrors) {
       core.setFailed(`Action failed with ${countErrors} errors (and ${countWarnings} warnings)`);
